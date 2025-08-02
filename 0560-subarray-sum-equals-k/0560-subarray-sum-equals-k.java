c143375210b24c7621ab5f1dbfdefd6b283b6545
aayushchourasia123 <142530266+aayushchourasia123@users.noreply.github.com>
2025-08-02 13:35:42 +0530

    Time: 25 ms (47.05%), Space: 45.9 MB (96.55%) - LeetHub

---
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        HashMap<Integer, Integer> mp=new HashMap();
+        mp.put(0,1); //frequncy of 0 is 1
+        int presum=0,cnt=0 ;
+        for(int i=0;i<nums.length;i++){
+            presum+=nums[i];
+            int remove=presum-k;//calulating x-k
+            
+    //if remove exit then cnt will add with frequency of remove else 0 will be added
+            cnt+=mp.getOrDefault(remove,0);
+            
+            //put presum in hashmap if already exit its frequency will be increase 
+            mp.put(presum,mp.getOrDefault(presum,0) + 1); 
+        }
+        return cnt;
+
+    }
+}
