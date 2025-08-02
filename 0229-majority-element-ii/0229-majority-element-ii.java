d16d5b0f1dcd9e32f2183df8c37f136e67e39de2
aayushchourasia123 <142530266+aayushchourasia123@users.noreply.github.com>
2025-08-03 02:02:24 +0530

    Time: 3 ms (76.61%), Space: 50.5 MB (27.35%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,33 +1,75 @@
 class Solution {
+    // public List<Integer> majorityElement(int[] nums) {
+
+    //     //tc=O(n logn)
+    //     //sc=O(1) (excluding result list)
+    //     if(nums.length==1) return  Arrays.asList(nums[0]);
+        
+    //     if(nums.length==2){
+    //         if(nums[0]==nums[1])
+    //         return  Arrays.asList(nums[0]);
+    //         else return Arrays.asList(nums[0],nums[1]);
+    //     }
+        
+
+    //     List<Integer> ans=new ArrayList<>();
+    //     Arrays.sort(nums);
+    //     int var=nums[0];
+    //     int count=1;
+    //     for(int i=1;i<nums.length;i++){
+    //         if(nums[i-1]==nums[i])
+    //         {
+    //             count++;
+    //         }
+    //         else{
+    //             var=nums[i];
+    //             count=1;
+    //         }
+    //         //!ans.contains(var) this to handle duplicates since duplicates to be added once only
+    //         if(count>nums.length/3 && !ans.contains(var)){ 
+    //             ans.add(var);
+    //         }
+
+    //     }
+    //     return ans;
+    // }
     public List<Integer> majorityElement(int[] nums) {
-        if(nums.length==1) return  Arrays.asList(nums[0]);
-        
-        if(nums.length==2){
-            if(nums[0]==nums[1])
-            return  Arrays.asList(nums[0]);
-            else return Arrays.asList(nums[0],nums[1]);
-        }
-        
+        //tc=O(n)
+        //sc=O(1)
+        int cnt1=0,cnt2=0;
+        int el1=Integer.MIN_VALUE;
+        int el2=Integer.MIN_VALUE;
+        for(int i=0;i<nums.length;i++){
 
-        List<Integer> ans=new ArrayList<>();
-        Arrays.sort(nums);
-        int var=nums[0];
-        int count=1;
-        for(int i=1;i<nums.length;i++){
-            if(nums[i-1]==nums[i])
-            {
-                count++;
+            if(cnt1==0 && el2!=nums[i]){
+                cnt1=1;
+                el1=nums[i];
             }
+            else if(cnt2==0 && el1!=nums[i]){
+                cnt2=1;
+                el2=nums[i];
+            }
+            else if(el1==nums[i]) cnt1++;
+            else if(el2==nums[i]) cnt2++;
+
             else{
-                var=nums[i];
-                count=1;
+                cnt1--;
+                cnt2--;
             }
-            //!ans.contains(var) this to handle duplicates since duplicates to be added once only
-            if(count>nums.length/3 && !ans.contains(var)){ 
-                ans.add(var);
-            }
-
         }
-        return ans;
+     // Final verification
+    cnt1 = 0;
+    cnt2 = 0;
+    for (int num : nums) {
+        if (num == el1) cnt1++;
+        else if (num == el2) cnt2++;
+    }
+
+    List<Integer> res = new ArrayList<>();
+    int n = nums.length;
+    if (cnt1 > n / 3) res.add(el1);
+    if (cnt2 > n / 3) res.add(el2);
+
+    return res;
     }
 }
