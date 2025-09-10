ce51562512fa37c39e52ba6d31d54545aaf5141c
aayushchourasia123 <142530266+aayushchourasia123@users.noreply.github.com>
2025-09-10 14:28:58 +0530

    Time: 0 ms (100%), Space: 43.2 MB (83.26%) - LeetHub

---
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean search(int[] nums, int target) {
+        int l=0,h=nums.length-1;
+
+       while(l<=h){
+        int mid=(l+h)/2;
+
+        if(nums[mid]==target) return true;
+        if(nums[mid]==nums[l] && nums[mid]==nums[h]){ //edge case for duplicate values
+            l=l+1;
+            h=h-1;
+            continue;
+        }
+
+        if(nums[l]<=nums[mid]){//left half sorted
+            if(nums[l]<=target && target<=nums[mid]){ //if lies target lies in left half
+                h=mid-1;
+            }
+            else l=mid+1;
+        } 
+        if(nums[mid]<=nums[h]){//right half sorted
+            if(nums[mid]<=target && target<=nums[h]){ //if lies target lies in right half
+                l=mid+1;
+            }
+            else h=mid-1;
+        }
+       }
+       return false;
+    }
+}
