8a6a9d2af074ccf67852e898453c6a83eebe7fd0
aayushchourasia123 <142530266+aayushchourasia123@users.noreply.github.com>
2025-09-24 18:18:55 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Kth Smallest/kth-smallest.java
+++ Difficulty: Medium/Kth Smallest/kth-smallest.java
@@ -3,14 +3,31 @@
 class Solution {
     public static int kthSmallest(int[] arr, int k) {
         // Your code here
-        PriorityQueue<Integer> mh=new PriorityQueue<>();
         
-        for(int no :arr){
-            mh.add(no);
+        //tc=O(n + k log n)
+//         PriorityQueue<Integer> mh=new PriorityQueue<>();
+        
+//         for(int no :arr){
+//             mh.add(no);
+//         }
+//         for(int i=0;i<k-1;i++){
+//             mh.poll();
+//         }
+//         return mh.peek();
+//     }
+// }
+
+//tc=O(n logk)
+PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        
+        for (int num : arr) {
+            pq.offer(num);
+            if (pq.size() > k) {
+                pq.poll(); // remove largest
+            }
         }
-        for(int i=0;i<k-1;i++){
-            mh.poll();
-        }
-        return mh.peek();
+        return pq.peek(); // root is kth smallest
     }
 }
+
+
