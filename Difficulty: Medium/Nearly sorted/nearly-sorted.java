29eaf41375d90e6913820adb241a6036b3b1d1dc
aayushchourasia123 <142530266+aayushchourasia123@users.noreply.github.com>
2025-09-24 19:34:39 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Nearly sorted/nearly-sorted.java
@@ -0,0 +1,17 @@
+class Solution {
+    public void nearlySorted(int[] arr, int k) {
+        // code here
+        PriorityQueue<Integer> pq=new PriorityQueue<>();
+        int idx=0;
+        for(int i=0;i<arr.length;i++){
+            
+            pq.add(arr[i]);
+            if(pq.size()==k+1){ //store element till k+1 in heap
+                arr[idx++]=pq.poll();
+            }
+        } 
+        while(!pq.isEmpty()){
+                arr[idx++]=pq.poll();
+            }
+    }
+}
