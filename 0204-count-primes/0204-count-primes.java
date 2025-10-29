d0c16dbcc13b9617765884de6e3788975ed78959
aayushchourasia123 <142530266+aayushchourasia123@users.noreply.github.com>
2025-10-29 19:19:44 +0530

    Time: 117 ms (15.47%), Space: 64.4 MB (7.97%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int countPrimes(int n) {
+        if(n<=1) return 0;
+        int prime[]=new int[n+1];
+        Arrays.fill(prime,1);
+        for(int i=2;i*i<=n;i++){
+            if(prime[i]==1){
+                for(int j=i*i;j<=n;j+=i){
+                    prime[j]=0;
+                }
+            }
+        }
+        int count=0; 
+        for(int i=2;i<n;i++){
+            if(prime[i]==1) count++;
+        }
+        return count;
+    }
+}
