5a3a4c065a12af60b428d82a456a5556d05b9ec4
aayushchourasia123 <142530266+aayushchourasia123@users.noreply.github.com>
2025-10-29 19:21:06 +0530

    Time: 116 ms (16.21%), Space: 64.2 MB (10.02%) - LeetHub

--- 0204-count-primes/0204-count-primes.java
+++ 0204-count-primes/0204-count-primes.java
@@ -1,9 +1,10 @@
 class Solution {
     public int countPrimes(int n) {
+        //tc=O(n)+O(n log log n)+O(n)
         if(n<=1) return 0;
         int prime[]=new int[n+1];
-        Arrays.fill(prime,1);
-        for(int i=2;i*i<=n;i++){
+        Arrays.fill(prime,1);//O(n)
+        for(int i=2;i*i<=n;i++){//O(n log log n)
             if(prime[i]==1){
                 for(int j=i*i;j<=n;j+=i){
                     prime[j]=0;
@@ -11,7 +12,7 @@ class Solution {
             }
         }
         int count=0; 
-        for(int i=2;i<n;i++){
+        for(int i=2;i<n;i++){//O(n)
             if(prime[i]==1) count++;
         }
         return count;
