a77c9d8f08993bbc1d1b2d4fdb13acc4da4c178f
AAYUSH CHOURASIA <142530266+aayushchourasia123@users.noreply.github.com>
2026-01-23 01:05:23 +0530

    Time: 0 ms (100%), Space: 46.6 MB (60.72%) - LeetHub

---
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
@@ -0,0 +1,59 @@
+/*
+// Definition for a Node.
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
+*/
+
+class Solution {
+    //tc=O(3n) sc=O(n)
+    public void insertCopyInBetween(Node head){
+        Node temp=head;
+        while(temp!=null){
+            Node nextEle=temp.next;
+            Node copyNode=new Node(temp.val);
+            copyNode.next=temp.next;
+            temp.next=copyNode;
+            temp=nextEle;
+        }
+    }
+    public void connectRandomPointer(Node head){
+        Node temp=head;
+        while(temp!=null){
+            Node copyNode=temp.next;
+            if(temp.random!=null){
+                copyNode.random=temp.random.next;
+            }
+            else{
+                copyNode.random=null;
+            }
+            temp=temp.next.next;
+        }
+    }
+    public Node getDeepCopy(Node head){
+        Node dummy=new Node(-1);
+        Node res=dummy;
+        Node temp=head;
+        while(temp!=null){
+            res.next=temp.next;
+            temp.next=temp.next.next;
+
+            res=res.next;
+            temp=temp.next;
+        }
+        return dummy.next;
+    }
+    public Node copyRandomList(Node head) {
+        insertCopyInBetween(head);
+        connectRandomPointer(head);
+        return getDeepCopy(head);
+    }
+}
