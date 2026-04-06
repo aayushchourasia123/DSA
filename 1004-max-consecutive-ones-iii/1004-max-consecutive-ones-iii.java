44dbf797277a4887f7032f46cc503f16291e7bbd
AAYUSH CHOURASIA <142530266+aayushchourasia123@users.noreply.github.com>
2026-04-06 22:29:22 +0530

    Time: 3 ms (97.59%), Space: 52.2 MB (43.94%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
@@ -1,44 +1,59 @@
 class Solution {
     public int longestOnes(int[] nums, int k) {
         
-        if(k==0){
-            int maxlen=0;
-            int count=0;
-            for(int i:nums){
-                if(i==1){
-                    count++;
-                    maxlen=Math.max(maxlen,count);
-                }
-                else count=0;
-            }
-            return maxlen;
-        }
-        boolean mark_0[]=new boolean[nums.length];
-        int l=0,r=0;
-        int maxlen=0;
-        while(r<nums.length){
-            if(nums[r]==0&&k!=0){
-                mark_0[r]=true;
-                k--;
-            }
-            else if(nums[r]==0&&k==0){
+        // if(k==0){
+        //     int maxlen=0;
+        //     int count=0;
+        //     for(int i:nums){
+        //         if(i==1){
+        //             count++;
+        //             maxlen=Math.max(maxlen,count);
+        //         }
+        //         else count=0;
+        //     }
+        //     return maxlen;
+        // }
+        // boolean mark_0[]=new boolean[nums.length];
+        // int l=0,r=0;
+        // int maxlen=0;
+        // while(r<nums.length){
+        //     if(nums[r]==0&&k!=0){
+        //         mark_0[r]=true;
+        //         k--;
+        //     }
+        //     else if(nums[r]==0&&k==0){
                 
-                while(!mark_0[l]&&l<=r){
-                    l++;
-                }
-                if(mark_0[l]==true){
-                    mark_0[l]=false;
-                    l++;
-                    k++;
-                    if(nums[r]==0&&k!=0){
-                        mark_0[r]=true;
-                        k--;
-                    }
-                }
+        //         while(!mark_0[l]&&l<=r){
+        //             l++;
+        //         }
+        //         if(mark_0[l]==true){
+        //             mark_0[l]=false;
+        //             l++;
+        //             k++;
+        //             if(nums[r]==0&&k!=0){
+        //                 mark_0[r]=true;
+        //                 k--;
+        //             }
+        //         }
+        //     }
+        //     maxlen=Math.max(maxlen,r-l+1);
+        //     r++;
+        // }
+        // return maxlen;
+
+        //tc=O(n) //sc=O(1)
+        int l=0;
+        int zeroCount=0;
+        int maxLen=0;
+        for(int r=0;r<nums.length;r++){
+            if(nums[r]==0) zeroCount++;
+
+            while(zeroCount>k){
+                if(nums[l]==0) zeroCount--;
+                l++;
             }
-            maxlen=Math.max(maxlen,r-l+1);
-            r++;
+            maxLen=Math.max(maxLen,r-l+1);
         }
-        return maxlen;
+        return maxLen;
     }
 }
