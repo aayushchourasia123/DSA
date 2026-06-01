9cbd733df54d800d92eeffe4df35537b09076ef1
AAYUSH CHOURASIA <142530266+aayushchourasia123@users.noreply.github.com>
2026-06-01 11:53:32 +0530

    Time: 0 ms (100%), Space: 43.4 MB (51.08%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.java
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -1,19 +1,18 @@
 class Solution {
-    //tc=(n^2)
+    //tc=O(n^2)
     public List<Integer> ncr(int row){
-        ArrayList<Integer> temp=new ArrayList<>();
+        List<Integer> temp=new ArrayList<>();
         temp.add(1);
         int ans=1;
         for(int col=1;col<row;col++){
-            ans=ans*(row-col);
-            ans=ans/col;
+            ans*=(row-col);
+            ans/=col;
             temp.add(ans);
         }
         return temp;
     }
     public List<List<Integer>> generate(int numRows) {
         List<List<Integer>> ans=new ArrayList<>();
-        
         for(int i=1;i<=numRows;i++){
             ans.add(ncr(i));
         }
