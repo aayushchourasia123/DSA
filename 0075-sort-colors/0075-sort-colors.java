059652a31608e3fab8a57c6f8e6dd5d052e5e660
AAYUSH CHOURASIA <142530266+aayushchourasia123@users.noreply.github.com>
2026-06-03 21:14:29 +0530

    Time: 0 ms (100%), Space: 43.7 MB (9.58%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.java
@@ -0,0 +1,27 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        //dutch national flag alogorithm
+        //tc=O(n) sc=O(1)
+        int low=0,mid=0,high=nums.length-1;
+
+        while(mid<=high){
+
+            if(nums[mid]==0){
+                int temp=nums[low];
+                nums[low]=nums[mid];
+                nums[mid]=temp;
+                low++;
+                mid++;
+            }
+            else if(nums[mid]==1){
+                mid++;
+            }
+            else{ //nums[mid]==2
+                int temp=nums[high];
+                nums[high]=nums[mid];
+                nums[mid]=temp;
+                high--;
+            }
+        }
+    }
+}
