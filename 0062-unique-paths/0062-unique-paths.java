4e3744c8ee4393863502f44e7ebf0965006bcf69
AAYUSH CHOURASIA <142530266+aayushchourasia123@users.noreply.github.com>
2026-06-07 01:26:47 +0530

    Time: 0 ms (100%), Space: 42.1 MB (64.2%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -21,23 +21,45 @@
 //     }
 // }
 
+// class Solution {
+//     //tabulation bottom up
+//     public int uniquePaths(int m, int n) {
+//         //tc=O(m*n) sc=O(m*n)
+//         int dp[][]=new int[m][n];
+//         dp[0][0]=1;
+//         for(int i=0;i<m;i++){
+//             for(int j=0;j<n;j++){
+//                 if(i==0 && j==0) continue;
+//                 else{
+//                     int up=0,left=0;
+//                     if(i>0)  up=dp[i-1][j];
+//                     if(j>0)  left=dp[i][j-1];
+//                     dp[i][j]=up+left;
+//                 }
+//             }
+//         }
+//         return dp[m-1][n-1];
+//     }
+// }
+
 class Solution {
-    //tabulation bottom up
+    //space optimization
     public int uniquePaths(int m, int n) {
-        //tc=O(m*n) sc=O(m*n)
-        int dp[][]=new int[m][n];
-        dp[0][0]=1;
+        //tc=O(n*m) sc=O(n)
+        int dp[]=new int[n];
         for(int i=0;i<m;i++){
+            int temp[]=new int[n];
             for(int j=0;j<n;j++){
-                if(i==0 && j==0) continue;
+                if(i==0 && j==0) temp[j]=1;
                 else{
-                    int up=0,left=0;
-                    if(i>0)  up=dp[i-1][j];
-                    if(j>0)  left=dp[i][j-1];
-                    dp[i][j]=up+left;
+                    int up=(i>0)? dp[j]:0;
+                    int left=(j>0)? temp[j-1]:0;
+
+                    temp[j]=up+left;
                 }
             }
+            dp=temp;
         }
-        return dp[m-1][n-1];
+        return dp[n-1];
     }
 }
