d8d2ae7b0eec39ca4e2b8af356b5fc7edf153530
AAYUSH CHOURASIA <142530266+aayushchourasia123@users.noreply.github.com>
2026-06-06 10:44:28 +0530

    Time: 0 ms (100%), Space: 47.8 MB (71.02%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,24 +1,22 @@
 class Solution {
-    //TC=O(log n)
     public double myPow(double x, int n) {
-      long N=n;
-      if(N<0){
-        x=1/x;
-        N=-N;
-      }
-       
-      double ans=1;
+        //tc=O(log n)
+        long N=n;
+        if(N<0){
+            x=1/x;
+            N=-N;
+        }
+        double ans=1.0;
         while(N>0){
             if(N%2==1){
                 ans*=x;
                 N-=1;
             }
             else{
-                N/=2;
                 x*=x;
+                N/=2;
             }
         }
-        
         return ans;
     }
 }
