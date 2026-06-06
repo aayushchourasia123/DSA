15f7c1ae5443f0455c634ab01290a05734d96e56
AAYUSH CHOURASIA <142530266+aayushchourasia123@users.noreply.github.com>
2026-06-07 00:58:53 +0530

    Time: 1 ms (13.72%), Space: 42.1 MB (64.06%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,21 +1,43 @@
-class Solution {
-    public int func(int i,int j,int dp[][]){
-        if(i==0 && j==0) return 1;
-        if(i<0 || j<0) return 0;
+// class Solution {
+//     public int func(int i,int j,int dp[][]){
+//         if(i==0 && j==0) return 1;
+//         if(i<0 || j<0) return 0;
 
-        if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
-        int up=func(i-1,j,dp);
-        int left=func(i,j-1,dp);
-        dp[i][j]=up+left;
-        return up+left;
-    }
+//         if(dp[i][j]!=-1){
+//             return dp[i][j];
+//         }
+//         int up=func(i-1,j,dp);
+//         int left=func(i,j-1,dp);
+//         dp[i][j]=up+left;
+//         return up+left;
+//     }
+//     public int uniquePaths(int m, int n) {
+//         //tc=O(2^m*n) sc=O(m-1 + n-1)
+//         int dp[][]=new int[m][n];
+//         for(int row[]:dp){
+//             Arrays.fill(row,-1);
+//         }
+//         return func(m-1,n-1,dp);
+//     }
+// }
+
+class Solution {
+    //tabulation bottom up
     public int uniquePaths(int m, int n) {
+        //tc=O(m*n) sc=O(m*n)
         int dp[][]=new int[m][n];
-        for(int row[]:dp){
-            Arrays.fill(row,-1);
+        dp[0][0]=1;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0) continue;
+                else{
+                    int up=0,left=0;
+                    if(i>0)  up=dp[i-1][j];
+                    if(j>0)  left=dp[i][j-1];
+                    dp[i][j]=up+left;
+                }
+            }
         }
-        return func(m-1,n-1,dp);
+        return dp[m-1][n-1];
     }
 }
