2361411543e18f3de68c8392580f80e482c38de0
AAYUSH CHOURASIA <142530266+aayushchourasia123@users.noreply.github.com>
2026-06-08 15:18:51 +0530

    Time: 21 ms (39.72%), Space: 45.6 MB (75.98%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,6 +1,6 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        //TC=O(n^4)
+       //TC=O(n^4)
         // int n=nums.length;
         // List<List<Integer>> ans=new ArrayList<>();
         // for(int i=0;i<n;i++){
@@ -21,29 +21,25 @@ class Solution {
         //     }
         // }
         // return ans;
-        
-        //tc=O(n^3)
-        //sc=O(1)
+
+        //tc=O(n^3) sc=O(1)->not using any extra space to solve problem but using space to return answer
         int n=nums.length;
-        List<List<Integer>> ans=new ArrayList<>();
         Arrays.sort(nums);
-        
+        List<List<Integer>> ans=new ArrayList<>();
         for(int i=0;i<n;i++){
             if(i>0 && nums[i]==nums[i-1]) continue;
-
             for(int j=i+1;j<n;j++){
-                if(j>i+1 && nums[j]==nums[j-1]) continue;
-
+                if(j!=i+1 && nums[j]==nums[j-1]) continue;
                 int k=j+1;
                 int l=n-1;
                 while(k<l){
                     long sum=(long)nums[i]+nums[j]+nums[k]+nums[l];
                     if(sum==target){
-                         ans.add(Arrays.asList(nums[i], nums[j], nums[k],nums[l]));
-                         k++;
-                         l--;
-                         while(k<l && nums[k]==nums[k-1]) k++;
-                         while(k<l && nums[l]==nums[l+1]) l--;
+                        ans.add(Arrays.asList(nums[i],nums[j],nums[k],nums[l]));
+                        k++;
+                        l--;
+                        while(k<l && nums[k]==nums[k-1]) k++;
+                        while(k<l && nums[l]==nums[l+1]) l--;
                     }
                     else if(sum<target) k++;
                     else l--;
