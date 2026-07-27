218d532ac40732d863fbcb8290cf06af217dbbe1
AAYUSH CHOURASIA <142530266+aayushchourasia123@users.noreply.github.com>
2026-07-27 13:27:10 +0530

    Time: 0 ms (100%), Space: 41.8 MB (85.85%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -0,0 +1,16 @@
+class Solution {
+    //memomization
+    //tc=O(n)
+    //sc=O(n)
+    public int fibo(int n,int dp[]){
+        if(n<=1) return n;
+        if(dp[n]!=0) return dp[n];
+        int ans=fibo(n-1,dp)+fibo(n-2,dp);
+        dp[n]=ans;
+        return ans;
+    }
+    public int fib(int n) {
+        int dp[]=new int[n+1];
+        return fibo(n,dp);
+    }
+}
