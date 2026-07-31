e8e224d5daa02f31597171fdcbbfd01cc724c4ea
AAYUSH CHOURASIA <142530266+aayushchourasia123@users.noreply.github.com>
2026-07-31 17:00:03 +0530

    Time: 0 ms (100%), Space: 42.1 MB (54.11%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
@@ -4,16 +4,31 @@ class Solution {
     //      if(n==1||n==2) return 1;
     //      return tribonacci(n-1)+tribonacci(n-2)+tribonacci(n-3);
     // }
+    // public int tribonacci(int n) {
+    //     //tc=O(n) sc=O(n)
+    //      if(n<=0) return 0;
+    //      if(n==1||n==2) return 1;
+
+    //      int dp[]=new int[n+1];
+
+    //      dp[0]=0;dp[1]=1;dp[2]=1;
+    //      for(int i=3;i<=n;i++){
+    //         dp[i]=dp[i-1]+dp[i-2]+dp[i-3];
+    //      }
+    //      return dp[n];
+    // }
     public int tribonacci(int n) {
-         if(n<=0) return 0;
-         if(n==1||n==2) return 1;
+        if(n<=0) return 0;
+        if(n==1||n==2) return 1;
 
-         int dp[]=new int[n+1];
-
-         dp[0]=0;dp[1]=1;dp[2]=1;
-         for(int i=3;i<=n;i++){
-            dp[i]=dp[i-1]+dp[i-2]+dp[i-3];
-         }
-         return dp[n];
+        int t0=0,t1=1,t2=1;
+        int sum=0;
+        for(int i=3;i<=n;i++){
+            sum=t0+t1+t2;
+            t0=t1;
+            t1=t2;
+            t2=sum;
+        }
+        return sum;
     }
 }
