5bfa695d16cd189e32b766dcd04d68800345d2ec
AAYUSH CHOURASIA <142530266+aayushchourasia123@users.noreply.github.com>
2026-08-01 16:43:37 +0530

    Time: 0 ms (100%), Space: 42.8 MB (57.5%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,14 +1,27 @@
 class Solution {
-    public int amt(int nums[],int i,int dp[]){
-        if(i>=nums.length) return 0;
-        if(dp[i]!=-1) return dp[i];
-        int take=nums[i]+amt(nums,i+2,dp);
-        int skip=amt(nums,i+1,dp);
-        return dp[i]=Math.max(take,skip);
-    }
+    // public int amt(int nums[],int i,int dp[]){
+    //     if(i>=nums.length) return 0;
+    //     if(dp[i]!=-1) return dp[i];
+    //     int take=nums[i]+amt(nums,i+2,dp);
+    //     int skip=amt(nums,i+1,dp);
+    //     return dp[i]=Math.max(take,skip);
+    // }
+    // public int rob(int[] nums) {
+    //     int dp[]=new int[nums.length]; //i varies from 0 to n-1
+    //     Arrays.fill(dp,-1);
+    //     return amt(nums,0,dp);
+    // }
+
+    //tabulation tc=O(n) sc=O(n)
     public int rob(int[] nums) {
-        int dp[]=new int[nums.length]; //i varies from 0 to n-1
-        Arrays.fill(dp,-1);
-        return amt(nums,0,dp);
+        int n=nums.length;
+        if(n==1) return nums[0];
+        int dp[]=new int[n];
+        dp[0]=nums[0];
+        dp[1]=Math.max(nums[0],nums[1]);
+        for(int i=2;i<n;i++){
+            dp[i]=Math.max(nums[i]+dp[i-2],dp[i-1]);
+        }
+        return dp[n-1];
     }
 }
