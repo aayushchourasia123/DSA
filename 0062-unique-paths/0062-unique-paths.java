9947628d144afb0a4ccc9765c3729d09295417e8
AAYUSH CHOURASIA <142530266+aayushchourasia123@users.noreply.github.com>
2026-08-06 13:42:57 +0530

    Time: 0 ms (100%), Space: 41.2 MB (99.91%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,65 +1,36 @@
-// class Solution {
-//     public int func(int i,int j,int dp[][]){
-//         if(i==0 && j==0) return 1;
-//         if(i<0 || j<0) return 0;
-
-//         if(dp[i][j]!=-1){
-//             return dp[i][j];
-//         }
-//         int up=func(i-1,j,dp);
-//         int left=func(i,j-1,dp);
-//         dp[i][j]=up+left;
-//         return up+left;
-//     }
-//     public int uniquePaths(int m, int n) {
-//         //tc=O(2^m*n) sc=O(m-1 + n-1)
-//         int dp[][]=new int[m][n];
-//         for(int row[]:dp){
-//             Arrays.fill(row,-1);
-//         }
-//         return func(m-1,n-1,dp);
-//     }
-// }
-
-// class Solution {
-//     //tabulation bottom up
-//     public int uniquePaths(int m, int n) {
-//         //tc=O(m*n) sc=O(m*n)
-//         int dp[][]=new int[m][n];
-//         dp[0][0]=1;
-//         for(int i=0;i<m;i++){
-//             for(int j=0;j<n;j++){
-//                 if(i==0 && j==0) continue;
-//                 else{
-//                     int up=0,left=0;
-//                     if(i>0)  up=dp[i-1][j];
-//                     if(j>0)  left=dp[i][j-1];
-//                     dp[i][j]=up+left;
-//                 }
-//             }
-//         }
-//         return dp[m-1][n-1];
-//     }
-// }
-
 class Solution {
-    //space optimization
-    public int uniquePaths(int m, int n) {
-        //tc=O(n*m) sc=O(n)
-        int dp[]=new int[n];
-        for(int i=0;i<m;i++){
-            int temp[]=new int[n];
-            for(int j=0;j<n;j++){
-                if(i==0 && j==0) temp[j]=1;
-                else{
-                    int up=(i>0)? dp[j]:0;
-                    int left=(j>0)? temp[j-1]:0;
+    // public int uniquePaths(int m, int n) {
+    //     // With memoization:
+    //     // TC = O(m * n)
+    //     // SC = O(m * n) + O(m + n)  // DP + recursion stack
 
-                    temp[j]=up+left;
-                }
+    //     // Without memoization:
+    //     // TC = O(2^(m + n)) (exponential)
+    //     // SC = O(m + n)
+    //     int dp[][]=new int[m][n];
+    //     for(int row[]:dp){
+    //         Arrays.fill(row,-1);
+    //     }
+    //     return path(0,0,m,n,dp);
+    // }
+    // public int path(int row,int col,int m,int n,int dp[][]){
+    //     if(row>=m ||  col>=n) return 0;
+    //     if(row==m-1 && col==n-1) return 1;
+    //     if(dp[row][col]!=-1) return dp[row][col];
+    //     int rightWays=path(row,col+1,m,n,dp);
+    //     int downWays=path(row+1,col,m,n,dp);
+    //     return dp[row][col]=rightWays+downWays;
+    // }
+     public int uniquePaths(int m, int n) {
+        // TC = O(m * n)
+        // SC = O(m * n) 
+        int dp[][]=new int[m][n];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0||j==0) dp[i][j]=1;
+                else dp[i][j]=dp[i][j-1]+dp[i-1][j];
             }
-            dp=temp;
         }
-        return dp[n-1];
+        return dp[m-1][n-1];
     }
 }
