4da0877bd703076d4bf314b87eaa6889b44b17f9
AAYUSH CHOURASIA <142530266+aayushchourasia123@users.noreply.github.com>
2026-08-11 15:07:42 +0530

    Time: 197 ms (9.11%), Space: 44.5 MB (53.83%) - LeetHub

---
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -0,0 +1,23 @@
+class Solution {
+    //tc=O(n root(n)) sc=O(n)
+    public boolean isPerfect(int n){
+        int sqrt=(int)Math.sqrt(n);
+        return sqrt*sqrt == n;
+    }
+    public int minSq(int n,int dp[]){
+        if(isPerfect(n)) return 1;
+        if(dp[n]!=-1) return dp[n];
+
+        int min=Integer.MAX_VALUE;
+        for(int i=1;i*i<=n;i++){
+            int count=minSq(i*i,dp)+minSq(n-i*i,dp);
+            min=Math.min(min,count);
+        }
+        return dp[n]=min;
+    }
+    public int numSquares(int n) {
+        int dp[]=new int[n+1];
+        Arrays.fill(dp,-1);
+        return minSq(n,dp);
+    }
+}
