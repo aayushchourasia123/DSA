1504aa237d5161fea526f75873d6de56c093ffc9
AAYUSH CHOURASIA <142530266+aayushchourasia123@users.noreply.github.com>
2026-08-16 22:23:45 +0530

    Time: 9 ms (55.32%), Space: 46.7 MB (26.39%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,14 +1,27 @@
 class Solution {
-    public int ways(int i,int nums[],int target){
+    public int ways(int i,int nums[],int target,int dp[][],int offset){
         if(i==nums.length){
             if(target==0) return 1;
             else return 0;
         }
-        int add=ways(i+1,nums,target-nums[i]);
-        int sub=ways(i+1,nums,target+nums[i]);
-        return add+sub;
+        if(target < -offset || target > offset) return 0;
+
+        int index=offset+target;
+        if(dp[i][index]!=-1) return dp[i][index];
+        int add=ways(i+1,nums,target-nums[i],dp,offset);
+        int sub=ways(i+1,nums,target+nums[i],dp,offset);
+        return dp[i][index]=add+sub;
     }
     public int findTargetSumWays(int[] nums, int target) {
-        return ways(0,nums,target);
+        int sum=0;
+        for(int i=0;i<nums.length;i++){
+            sum+=nums[i];
+        }
+        if(target>sum || target<-sum) return 0;
+        int offset=sum;
+        //i=>0 to n-1 | target -> -sum to sum
+        int dp[][]=new int[nums.length][2*sum +1];
+        for(int row[]:dp) Arrays.fill(row,-1);
+        return ways(0,nums,target,dp,offset);
     }
 }
