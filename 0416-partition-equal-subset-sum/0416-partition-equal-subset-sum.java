3bac76680e9c96162f7e5746ef5ae1791b8403d5
AAYUSH CHOURASIA <142530266+aayushchourasia123@users.noreply.github.com>
2026-08-17 02:30:08 +0530

    Time: 86 ms (27.86%), Space: 76.1 MB (12.22%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,18 +1,18 @@
 class Solution {
     //tc=O(n*target) sc=O(n*target)
     public boolean subset(int i,int target,int nums[],int dp[][]){
-        if(i==nums.length){
+        if(i<0){
             if(target==0) return true;
             else return false;
         }
         if(dp[i][target]!=-1) return dp[i][target]==1;
         boolean ans=false;
-        boolean skip=subset(i+1,target,nums,dp);
+        boolean skip=subset(i-1,target,nums,dp);
         if(target-nums[i]<0){
             ans=skip;
         }
         else {
-            boolean pick=subset(i+1,target-nums[i],nums,dp);
+            boolean pick=subset(i-1,target-nums[i],nums,dp);
             ans= pick || skip;
         }
         dp[i][target]=(ans)?1:0;
@@ -27,11 +27,11 @@ class Solution {
         if(sum%2!=0) return false;
 
         int target=sum/2;
-        //i=0 to n-1 and target=target to 0;
+        //i=n-1 to 0 and target=target to 0;
         int dp[][]=new int[n][target+1];
         for(int row[] : dp){
             Arrays.fill(row,-1);
         }
-        return subset(0,target,nums,dp);
+        return subset(n-1,target,nums,dp);
     }
 }
