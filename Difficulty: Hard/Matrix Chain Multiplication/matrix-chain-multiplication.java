8d227eceb7e46f64fb274d7ff36744ba0708af1b
AAYUSH CHOURASIA <142530266+aayushchourasia123@users.noreply.github.com>
2026-08-29 12:27:47 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Matrix Chain Multiplication/matrix-chain-multiplication.java
@@ -0,0 +1,21 @@
+class Solution {
+    static int cost(int i,int j,int arr[],int dp[][]){
+        if(i==j) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+        int minCost=Integer.MAX_VALUE;
+        for(int k=i;k<j;k++){
+            int x=arr[i]*arr[k+1]*arr[j+1];
+            int totalCost=cost(i,k,arr,dp)+cost(k+1,j,arr,dp)+x;
+            minCost=Math.min(totalCost,minCost);
+        }
+        return dp[i][j]=minCost;
+    }
+    static int matrixMultiplication(int arr[]) {
+        // code here
+        int n=arr.length;
+        int i=0,j=n-2;
+        int dp[][]=new int[n-1][n-1];
+        for(int row[]:dp) Arrays.fill(row,-1);
+        return cost(i,j,arr,dp);
+    }
+}
